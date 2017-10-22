24a2b160f7d641d1dc4314546dc69f0e25b2f6e9
Charles <cgarzon132@gmail.com>
2017-10-22 01:53:14 -0500

    Finnished profile design

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -10,69 +10,111 @@
             <v-btn  absolute dark fab small class="grey"><v-icon>edit</v-icon></v-btn>
           </v-flex>
           <v-list-tile-avatar>
-            <img src="/static/thumbnail.jpg" style="height:125px;width:125px;" />
+            <img :src="userObj.profile_pic.thumbnail" class="profile-picture" />
           </v-list-tile-avatar>
-          <h4 style="margin-top:5px;margin-bottom:2px;">User Name</h4>
-          <p class="grey6-text">
-            La descripcion super duper chida de la
-            personita de la cual visitas el perfil,
-            maximo de N caracteres.
+          <h4 class="username">{{ userObj.username }}</h4>
+          <p v-if="userObj.bio" class="grey6-text">
+            {{ userObj.bio }}
           </p>
+          <p v-else class="grey6-text">
+            <i>No hay descripcion</i>
+          </p>
+        </div>
+        <br />
+        <div class="text-xs-center">
+          <v-btn round primary dark>agregar contacto<v-icon right>person_add</v-icon></v-btn>
+          <v-btn round error dark>eliminar contacto<v-icon right>remove_circle_outline</v-icon></v-btn>
         </div>
         <br />
         <!-- Social Networking -->
         <v-card>
           <v-card-title class="grey">
             <span class="headline white--text" style="font-size:25%;">Redes Sociales</span>
-            <v-spacer></v-spacer>
-            <v-btn icon class="white--text"><v-icon>edit</v-icon></v-btn>
           </v-card-title>
-          <v-card-text>
-            <v-list>
-              <v-list-tile>
-                Something
-              </v-list-tile>
-              <v-divider></v-divider>
-              <v-list-tile>
-                Something
-              </v-list-tile>
-            </v-list>
-          </v-card-text>
+          <v-list>
+            <v-list-tile>
+              <img src="static/icon-zuckd.svg" class="social-icon" />Facebook Handle
+            </v-list-tile>
+            <v-divider></v-divider>
+            <v-list-tile>
+              <img src="static/icon-instagram.svg" class="social-icon" />Instagram Handle
+            </v-list-tile>
+            <v-divider></v-divider>
+            <v-list-tile>
+              <img src="static/icon-snapchat.svg" class="social-icon" />Snapchat Handle
+            </v-list-tile>
+            <v-divider></v-divider>
+            <v-list-tile>
+              <img src="static/icon-twitter.svg" class="social-icon" />Twitter Handle
+            </v-list-tile>
+          </v-list>
         </v-card>
         <br />
         <!-- Account Settings -->
         <v-card>
           <v-card-title class="orange">
-            <span class="headline white--text" style="font-size:25%;">Info de Cuenta</span>
+            <span class="headline white--text" style="font-size:25%;">Cuenta</span>
           </v-card-title>
           <v-card-text>
-            <v-list>
-              <v-list-tile>
-                Something
-              </v-list-tile>
-              <v-divider></v-divider>
-              <v-list-tile>
-                Something
-              </v-list-tile>
-            </v-list>
+            <v-container fluid>
+              <!-- Account Status -->
+              <v-layout row-sm column child-flex-sm>
+                <v-flex xs6>
+                  <span class="label-desc">Estatus:</span>
+                  <v-chip class="red white--text">Banneado</v-chip>
+                  <v-chip class="green white--text">Activo</v-chip>
+                </v-flex>
+                <v-flex xs6>
+                  <span class="label-desc">Ultimo login:</span>
+                  <v-chip class="secondary white--text">2017-10-22T03:45:04.226Z</v-chip>
+                </v-flex>
+              </v-layout>
+              <br />
+              <!-- Anonimity Status -->
+              <v-layout row-sm column child-flex-sm>
+                <v-flex xs12>
+                  <span class="label-desc">Anonimidad:</span>
+                  <v-switch class="switch-wrapper" label="Estado: Activo" ></v-switch>
+                </v-flex>
+              </v-layout>
+            </v-container>
           </v-card-text>
         </v-card>
         <br />
         <!-- Account Settings -->
         <v-card>
           <v-card-title class="red">
-            <span class="headline white--text" style="font-size:25%;">Settings</span>
+            <span class="headline white--text" style="font-size:25%;">Seguridad</span>
           </v-card-title>
           <v-card-text>
-            <v-list>
-              <v-list-tile>
-                Something
-              </v-list-tile>
-              <v-divider></v-divider>
-              <v-list-tile>
-                Something
-              </v-list-tile>
-            </v-list>
+            <v-container fluid>
+              <!-- EMAIL -->
+              <v-layout row-sm column child-flex-sm>
+                <v-flex xs10-12>
+                  <v-text-field
+                  name="input-1"
+                  label="email@email.com"
+                  id="testing"
+                  ></v-text-field>
+                </v-flex>
+                <v-flex xs2>
+                  <v-btn right round primary dark class="button-wrapper">actualizar</v-btn>
+                </v-flex>
+              </v-layout>
+              <!-- PASSWORD -->
+              <v-layout row-sm column child-flex-sm>
+                <v-flex xs10-12>
+                  <v-text-field
+                  name="input-1"
+                  label="************"
+                  id="testing"
+                  ></v-text-field>
+                </v-flex>
+                <v-flex xs2>
+                  <v-btn right round primary dark class="button-wrapper">actualizar</v-btn>
+                </v-flex>
+              </v-layout>
+            </v-container>
           </v-card-text>
         </v-card>
         <br /><br />
@@ -88,16 +130,24 @@ export default {
   data () {
     return {
       profileId: null,
-      profileData: null
+      profileData: null,
+      userObj: null,
+      anonimityState: false
     }
   },
   created () {
     this.profileId = this.$route.params.profileId
+    this.loadProfileInfo()
   },
   methods: {
-    loadProfileInfo (profileId) {
-      if (profileId === 'me') {
-        // Request profile info from server
+    loadProfileInfo () {
+      if (this.profileId === 'me') {
+        // Get profile info from object in session if not load from server
+        if (this.$session.get('USER')) {
+          this.userObj = this.$session.get('USER')
+        } else {
+          // Load user from server
+        }
       } else {
         // Request profile info from server
       }
@@ -105,4 +155,32 @@ export default {
   }
 }
 </script>
-<style></style>
+
+<style scoped>
+.profile-picture {
+  height:125px;
+  width:125px;
+  background-color:grey;
+}
+.username {
+  margin-top:5px;
+  margin-bottom:2px;
+}
+.label-desc {
+  font-weight: bold;
+  margin-right:2px;
+}
+.social-icon {
+  min-height:30px;
+  min-width:30px;
+  margin:2px;
+  margin-right:5px;
+}
+.switch-wrapper {
+  margin-top:0px;
+  padding-top:0px;
+}
+.button-wrapper {
+  margin-top:15px;
+}
+</style>
