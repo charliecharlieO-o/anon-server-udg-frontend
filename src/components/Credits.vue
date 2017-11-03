2b7c6b11fac3b6c5f1d1c9d8c58ee0378ce82069
Charles <cgarzon132@gmail.com>
2017-11-03 17:19:55 -0600

    Finished credits to website component

--- src/components/Credits.vue
+++ src/components/Credits.vue
@@ -1,46 +1,37 @@
 <template>
   <div id="creditsMain">
-    <v-layout row style="margin-top:15px;">
+    <v-layout row style="margin-top:15px;padding:10px;">
       <v-flex xs12 style="text-align:center;padding:15px;">
         <h2>Agradecimientos</h2>
-        <h5>
-          Quiero agradecer a las personas que contribuyeron a la creación de este sitio ya
-          que sin ellos/ellas esta plataforma no existiría.
-        </h5>
         <v-divider style="margin-top:3px;margin-bottom:5px;"></v-divider>
       </v-flex>
     </v-layout>
     <v-layout row style="padding:15px;">
-      <v-flex xs12 style="text-align:left;">
-        <h6>
-          Este sitio fue inspirado por las limitaciones de libertad de expresión que existen en facebook, las
-          feminazis que acechan los grupos para silenciar a la oposición, CUCEINetwork, los litros y litros
-          de café del starbucks de forum, Yolitzi, las bellas elfas del CUCEI, el guardia predicador,
-          #NoEsNormal y una botella de DonJulio70.<br />
-          Con el propósito de crear un espacio de discurso único para todos los alumnos de la Universidad de
-          Guadalajara, sin la censura de las redes existentes y donde todos puedan expresar su opinión (buena o mala)
-          sobre lo que quieran.
-        </h6>
+      <v-flex xs12 style="text-align:left;padding-left:30px;padding-right:30px;">
+        <p>
+          Un agradecimiento y fuerte abrazo a las personas que contribuyeron a la creacion de este sitio, las
+          feministas radicales por su incansable lucha en contra de los rastrillos, los baristas del Starbucks
+          de plaza forum por proveerme del combustible csobre el cual fue hecho este sitio, QciSocial y el conglomerado
+          de buitres que ahi se hospedan, el guardia predicador por bendecir los pasillos en los que caminamos y guiarnos
+          por el camino apartado del articulo 35.<br /><br />
+          Este sitio existe con el propósito de crear un espacio de discurso único para todos los alumnos de la Universidad de
+          Guadalajara, sin censura y donde todos puedan expresar su opinión (buena o mala) sobre lo que quieran.
+        </p>
         <br />
         <h6>
-          Quiero agradecer especificamente a las siguientes personas por su contribución técnica al sitio:
-          <br /><br />
-          <ul>
-            <li>
-              Victor Agustín Diaz Mendez: por el modulo de conexión con la UdeG y consejos de UI.
-            </li>
-            <li>
-              Jesús Gabriel Medina Gonzalez: por el diseño y creación del logo del sitio.
-            </li>
-          </ul>
+          Agradecimientos especiales a:
         </h6>
-        <v-divider style="margin-top:3px;margin-bottom:5px;"></v-divider>
+        <ul>
+          <li>
+            Victor Agustín Diaz Mendez (cofundador y colaborador de UI)
+          </li>
+        </ul>
       </v-flex>
     </v-layout>
-    <v-layout row style>
+    <v-layout row style="margin-bottom:10%;margin-top:5%;">
       <v-flex xs12 style="text-align:center;">
         <h4>Espero disfruten el sitio como yo disfrute creandolo</h4>
-        <img src="../assets/thanks.gif" style="height:100%;width:95%;" />
+        <img src="../assets/thanks.gif" style="height:100%;width:95%;max-width:400px;" />
       </v-flex>
     </v-layout>
   </div>
